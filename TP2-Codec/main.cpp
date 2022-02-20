070d69f8db5ded78be54a586f1b34f55c0ffa8c4
Clain Dimitri <dimitri-jean.clain@epitech.eu>
2022-02-20 12:23:41 -0500

    Initial Commit

---
+++ TP2-Codec/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TP2-Codec
+//
+//  Created by Clain Dimitri on 2022-02-20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
