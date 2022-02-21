12b63772e747cae9010071483503d0883a1d5be0
Pandatec <louis.vandenbosssche@epitech.eu>
2022-02-21 18:40:17 -0500

    [ADD] add of the open file system and error gestion for the arguments

--- TP2-Codec/main.cpp
+++ TP2-Codec/main.cpp
@@ -6,253 +6,53 @@
 //
 
 #include <string>
-#include <fstream>
 #include <iostream>
-#include <cstring>
-#include <list>
+#include <fstream>
 
-using namespace std;
 
-int count = 1; //1 based
-int bitLoc = 0;//1 based
-bool nextByte = true;
-fstream stream;
-char b = 0x00;
-int fileLength = 0;
-bool doneFile = false;
-int origFileLength;
-int pos = 1;
 
-int getNextBit() {
-    if( bitLoc == 0 ) {
-        bitLoc = 8;
-        b = stream.get();
-        fileLength--;
-        if( fileLength < 0 ) {
-            doneFile = true;
-            return 0;
-        }
-    }
-    
-    int ret = b & (1 << (bitLoc-1));
-    ret = ret >> (bitLoc-1);
-    ret &= 0x00000001;
-    //printf("%.2X ", ret);
-    bitLoc--;
-    
-    return ret;
-}
-
-int getNextEncodedBit() {
-    
-    if( bitLoc == 0 ) {
-        
-        b = stream.get();
-        fileLength--;
-        
-        if( pos == 1 ) {
-            b = (b & 0x0F);
-            bitLoc = 4;
-        } else {
-            b = (b & 0x3F);
-            bitLoc = 6;
-        }
-        
-        pos++;
-        
-        if( pos == 4 ) {
-            pos = 1;
-        }
-        if( fileLength < 0 ) {
-            doneFile = true;
-            return 0;
-        }
-    }
-    
-    int ret = b & (1 << (bitLoc-1));
-    ret = ret >> (bitLoc-1);
-    ret &= 0x00000001;
-    //printf("%.2X ", ret);fflush(stdout);
-    bitLoc--;
-    
-    return ret;
-
-}
-
-int main( int argv, char **argc ) {
-    stream.open( argc[2], ios::binary | ios::in );
-    if( !stream.is_open() ) {
-        cerr << "Cannot open file: " << argc[2] << endl;
-        return -1;
-    }
-
-    string flag(argc[1]);
-    
-    if( flag == "-d" ) {
-        stream.seekg(0, stream.end);
-        fileLength = stream.tellg();
-        origFileLength = fileLength;
-        stream.seekg(0, stream.beg);
-
-        list<char> decoded;
-        char newByte = 0x00;
-        char newByteCount = 8;
-        bool validByte;
-        while(true) {
-                        
-            newByteCount = 8;
-            newByte = 0x00;
-
-            validByte = false;
-            while( newByteCount != 0 ) {
-                int b = getNextEncodedBit();
-
-                if( doneFile ) {
-                    break;
-                }
-
-                validByte = true;
-
-                newByte |= (b << (newByteCount-1));
-
-                newByteCount--;
-                
-            }
-        
-            if( validByte )
-            {
-                decoded.push_back(newByte);
-            }
-            
-            if( doneFile ) {
-                break;
-            }
-
-        }
-
-        if( origFileLength % 3 != 0 ) {
-            decoded.pop_back();
-            decoded.pop_back();
-
-            int realB = 0x00;
-            
-            stream.close();
-            stream.open( argc[2], ios::binary | ios::in );
-            stream.seekg(origFileLength-2,stream.beg);
-            char newB1 = stream.get();
-            char newB2 = stream.get();
-
-            realB |= (newB1 & 0x0F) << 4;
-            realB |= (newB2 & 0x3C) >> 2;
-
-            decoded.push_back(realB);
-        }
-
-        list<char>::iterator beg = decoded.begin();
-        list<char>::iterator end = decoded.end();
-        string outputFilename = string(argc[2]) + string(".decoded");
-        fstream output( outputFilename.c_str(), ios::binary | ios::out );
-        int lineBreak = 30;
-        while( beg != end ) {
-            printf( "%.2x ", *beg & 0xff );
-            output.put(*beg);
-            lineBreak--;
-            if( lineBreak == 0 ) {
-                lineBreak = 30;
-                printf("\n");
-            }
-            beg++;
-        }
-
-        printf("\n");
-    }
-    
-    if( flag == "-e" ) {
-
-        stream.seekg(0, stream.end);
-        fileLength = stream.tellg();
-        origFileLength = fileLength;
-        stream.seekg(0, stream.beg);
-        
-        list<char> encoded;
-        char b1 = 0xE0;
-        char bn = 0x80;
-        char newByte = 0;
-        char codePointCount = 1; //1 based
-        int newByteCount = 0;
-        bool encValid;
-        
-        while( 1 ) {
-
-            if( codePointCount == 1 ) {
-                newByte = b1;
-                newByteCount = 4;
-            } else {
-                newByte = bn;
-                newByteCount = 6;
-            }
-            
-            encValid = false;
-            while( newByteCount != 0 ) {
-                int b = getNextBit();
-                
-                if( doneFile ) {
-                    break;
-                }
-                encValid = true;
-                //Put b at newByte:newByteCount
-                newByte |= (b << (newByteCount-1));
-
-                newByteCount--;
-                
-            }
-            
-            if( encValid ) {
-                encoded.push_back(newByte);
-            }
-            
-            if( doneFile ) {
-                break;
-            }
-            
-            if( codePointCount == 3 ) {
-                codePointCount = 1;
-            } else {
-                codePointCount++;
-            }
-            
-        }
-        
-        if( origFileLength % 2 != 0 ) {
-            int newB2 = encoded.back();
-            encoded.pop_back();
-            int newB1 = encoded.back();
-            encoded.pop_back();
-
-            newB1 &= ~(1 << 5); //Clear bit 6
-            encoded.push_back(newB1);
-
-            encoded.push_back(newB2);
-        }
-
-        list<char>::iterator beg = encoded.begin();
-        list<char>::iterator end = encoded.end();
-        int lineBreak = 30;
-        string outputFilename = string(argc[2]) + string(".encoded");
-        fstream output( outputFilename.c_str(), ios::binary | ios::out );
-        while( beg != end ) {
-            output.put(*beg);
-            printf( "%.2x ", *beg & 0xff );
-            lineBreak--;
-            if( lineBreak == 0 ) {
-                lineBreak = 30;
-                printf("\n");
-            }
-            beg++;
-        }
-
-        printf("\n");
-    }
-    
+int UtfToUni() {
+    std::cout << "Utf To Uni" << std::endl;
     return 0;
+}
+
+int UniToUtf() {
+    std::cout << "Uni To Utf" << std::endl;
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    int res = 0;
+    if (argc < 2) {
+        res = 84;
+    } else if (argc == 2) {
+        std::string flag(argv[1]);
+        if (flag == "-h") {
+            std::cout << "HELP" << std::endl;
+            std::cout << std::endl;
+            std::cout << "Usage:" << std::endl;
+            res = 0;
+        } else {
+            res = 84;
+        }
+    } else if (argc > 3) {
+        res = 84;
+    } else {
+        std::fstream file;
+        file.open(argv[2], std::ios::binary | std::ios::in);
+        
+        if (!file.is_open()) {
+            std::cout << "Error while open file" << std::endl;
+            res = 84;
+        } else {
+            std::string flag(argv[1]);
+            if (flag == "+") {
+                res = UtfToUni();
+            } else if (flag == "-") {
+                res = UniToUtf();
+            }
+            file.close();
+        }
+    }
+    return res;
 }
