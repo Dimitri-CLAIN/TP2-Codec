0349c9e9b79ca3f0471784adf75bbf80f320c944
Clain Dimitri <dimitri-jean.clain@epitech.eu>
2022-02-25 01:50:22 -0500

    Add uff to uni

--- TP2-Codec/main.cpp
+++ TP2-Codec/main.cpp
@@ -10,8 +10,77 @@
 #include <fstream>
 #include <vector>
 
-int UtfToUni(char *text) {
-    //std::cout << "Utf To Uni" << std::endl;
+wchar_t getBytes(wchar_t value, int pos, int nbBytes)
+{
+    return ((1 << nbBytes) - 1) & (value >> (pos - 1));
+}
+
+wchar_t *utf8BytesToUnicodeBytes(wchar_t bytes, int nbBytes)
+{
+    int n = nbBytes;
+    std::vector<wchar_t> res(1, 0);
+
+    while (bytes & (1 << n)) {
+        n--;
+    }
+    n = (nbBytes - n) - 1;
+    switch (n) {
+        case 1:
+            res[0] = (getBytes(bytes, 9, 5) << 6) | (getBytes(bytes, 1, 6));
+            res.push_back(0b00000000);
+            res.push_back(0b00000000);
+            res.push_back(0b00000000);
+            break;
+        case 2:
+            res[0] = (getBytes(bytes, 17, 4) << 4) | (getBytes(bytes, 11, 4));
+            res.push_back((getBytes(bytes, 9, 2) << 6) | (getBytes(bytes, 1, 6)));
+            res.push_back(0b00000000);
+            res.push_back(0b00000000);
+            break;
+        case 3:
+            res[0] = (getBytes(bytes, 25, 3) << 2) | (getBytes(bytes, 21, 2));
+            res.push_back((getBytes(bytes, 17, 4) << 4) | (getBytes(bytes, 11, 4)));
+            res.push_back((getBytes(bytes, 9, 2) << 6) | (getBytes(bytes, 1, 6)));
+            res.push_back(0b00000000);
+            break;
+    }
+    res.push_back('\0');
+    return &res[0];
+}
+
+int UtfToUni(std::string text)
+{
+//    wchar_t bytes = 0b1100001010100011;
+//    wchar_t bytes = 0b111011110011111100111111;
+//    wchar_t bytes = 0b111000001010010010111001;
+//    wchar_t bytes = 0b111000101000001010101100;
+//    wchar_t bytes = 0b111011011001010110011100;
+//    wchar_t bytes = 0b11110000100100001000110110001000;
+    wchar_t *data = NULL;
+
+    std::wofstream myfile(L"example.encoded", std::ios_base::binary | std::ios_base::out);
+//    data = utf8BytesToUnicodeBytes(bytes, 15);
+//    data = utf8BytesToUnicodeBytes(bytes, 23);
+//    data = utf8BytesToUnicodeBytes(bytes, 31);
+//    myfile << data;
+
+    for (wchar_t nextChar : text) {
+        unsigned int len = (unsigned int)nextChar;
+        if (len == 0)
+            continue;
+        if (len < 163) {
+            std::vector<wchar_t> res(1, getBytes(nextChar, 1, 7));
+            data = &res[0];
+        }
+        else if (len < 2361)
+            data = utf8BytesToUnicodeBytes(nextChar, 15);
+        else if (len < 66376)
+            data = utf8BytesToUnicodeBytes(nextChar, 23);
+        else if (len >= 66376)
+            data = utf8BytesToUnicodeBytes(nextChar, 31);
+        myfile << data;
+    }
+    myfile.close();
     return 0;
 }
 
@@ -50,16 +119,17 @@ int UniToUtf(std::string text) {
     wchar_t *charUTF8 = NULL;
     std::wofstream myfile(L"example.encoded", std::ios_base::binary | std::ios_base::out);
 
+
     for (wchar_t nextChar : text) {
         if ((unsigned int)nextChar == 0)
             continue;
-        if ((unsigned int)nextChar <= 36)
+        if ((unsigned int)nextChar < 163)
             charUTF8 = unicodeBytesToUft8bytes(nextChar, 1);
-        else if ((unsigned int)nextChar <= 163)
+        else if ((unsigned int)nextChar < 2361)
             charUTF8 =unicodeBytesToUft8bytes(nextChar, 6);
-        else if ((unsigned int)nextChar <= 54620)
+        else if ((unsigned int)nextChar < 66376)
             charUTF8 =unicodeBytesToUft8bytes(nextChar, 12);
-        else if ((unsigned int)nextChar <= 66376)
+        else if ((unsigned int)nextChar >= 66376)
             charUTF8 =unicodeBytesToUft8bytes(nextChar, 18);
         myfile << charUTF8;
     }
@@ -75,7 +145,7 @@ char *reader(char *argv) {
     file.open(argv, std::ios::binary | std::ios::in);
     if (file.is_open()) {
         file.seekg(0, file.end);
-        fileLen = file.tellg();
+        fileLen = (int)file.tellg();
         file.seekg(0, file.beg);
         text = new char[fileLen];
         file.read(text, fileLen);
@@ -122,7 +192,7 @@ int main(int argc, char **argv) {
         if (text == NULL) {
             res = 84;
         } else {
-            std::cout.write(text, std::strlen(text)) << std::endl;
+//            std::cout.write(text, std::strlen(text)) << std::endl;
             if (flag1 == "+") {
                 res = UtfToUni(text);
             } else if (flag1 == "-") {
